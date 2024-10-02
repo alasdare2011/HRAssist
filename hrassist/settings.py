c81c7775ce2003bcf0ae8bbcd4d4c37c2d70d73b
Alasdair <afullerca@gmail.com>
2024-10-02 16:47:40 -0700

    created custom user model

--- hrassist/settings.py
+++ hrassist/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "employee_time_management.apps.EmployeeTimeManagementConfig",
+    # Local
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUser"
